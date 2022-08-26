faaae601fd9fbbbdb4759eb200a3e499ee06f0ea
Jordi <jbardia@ufl.edu>
2022-08-26 16:36:12 -0400

    Switching collapses with tabs

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,46 +1,11 @@
 import Projects from './projects';
 import Experience from './experience';
 import React, {useEffect, useState} from "react";
-import {Button, Navbar, Image, Container, Col, Row, Card, Collapse, CardDeck, ListGroup, ListGroupItem, Modal} from 'react-bootstrap';
+import {Navbar, Image, Container, Col, Row, Tab, Tabs} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
 const Homepage = () => {
-    const [openProjects, setOpenProjects] = useState(false);
-    const [openExperience, setOpenExperience] = useState(false);
-    const [openResume, setOpenResume] = useState(false);
-    //const [openAvatarModal, setOpenAvatarModal] = useState(false);
-    //const [openRRModal, setOpenRRModal] = useState(false);
-    //const [openNNModal, setOpenNNModal] = useState(false);
-
-    const switchContent = (param) => { //For instant switching if another selection is open
-        switch(param){
-            case 'projects':
-                {
-                    if (openResume){
-                        return 0;
-                    }
-                    return 300;
-                }
-            case 'experience':
-                {
-                    if (openExperience){
-                        return 0;
-                    }
-                    return 300;
-                }
-            case 'resume':
-                {
-                    if (openProjects){
-                        return 0;
-                    }
-                    return 300;
-                }
-            default:
-                return 0;
-        }
-    }
-
     return (
         <div style={{width:'100%', margin: 'auto'}}>
             <Container className = "homepageContainer">
@@ -63,34 +28,19 @@ const Homepage = () => {
                                 </a>
                             </div>
                         </Navbar>
-                        <Navbar>
-                            <div className = "site-links">
-                                <Button variant = "link" size = "lg" onClick={() => {setOpenProjects(false);
-                                                                                    setOpenExperience(false);
-                                                                                    setOpenResume(!openResume)}}>Resume</Button>
-                                <Button variant = "link" size = "lg" onClick={() => {setOpenProjects(false);
-                                                                                    setOpenExperience(!openExperience);
-                                                                                    setOpenResume(false);}}>Experience</Button>
-                                <Button variant = "link" size = "lg" onClick={() => {setOpenProjects(!openProjects);
-                                                                                    setOpenExperience(false);
-                                                                                    setOpenResume(false);}}>Projects</Button>
-                            </div>
-                        </Navbar>
-                        <Collapse in={openProjects} timeout = {switchContent('projects')}>
-                            <div>
-                                <Projects/>
-                            </div>
-                        </Collapse>
-                        <Collapse in={openExperience} timeout = {switchContent('experience')}>
-                            <div>
+                        <Tabs defaultActiveKey="resume" className="mb-3" justify={true}>
+                            <Tab eventKey="resume" title="Resume">
+                                <div className="resume-div">
+                                    <iframe src = "https://drive.google.com/file/d/1aq2rJm62fyxApQymHJfPUugdwAsEthmA/preview" width = "640" height = "480" className = "resume-iframe"/>
+                                </div>
+                            </Tab>
+                            <Tab eventKey="experience" title="Experience">
                                 <Experience/>
-                            </div>
-                        </Collapse>
-                        <Collapse in={openResume} timeout = {switchContent('resume')}>
-                            <div className="resume-div">
-                                <iframe src = "https://drive.google.com/file/d/1aq2rJm62fyxApQymHJfPUugdwAsEthmA/preview" width = "640" height = "480" className = "resume-iframe"/>
-                            </div>
-                        </Collapse>
+                            </Tab>
+                            <Tab eventKey="projects" title="Projects">
+                                <Projects/>
+                            </Tab>
+                        </Tabs>
                     </Col>
                 </Row>
             </Container>
